b84c52c745716f1d64bdc6a7c134c3644370fc86
nuts4nuts4nuts <nuts4nuts4nuts@gmail.com>
2015-10-10 01:13:32 -0400

    Test scaling canvas when the window is resized

--- testJSScript.js
+++ testJSScript.js
@@ -2,3 +2,19 @@ var canvas = document.getElementById("mainCanvas");
 var context = canvas.getContext("2d");
 context.fillStyle = "#FF0000";
 context.fillRect(0, 0, 150, 75);
+
+// When the window is resized we want to
+//resize the canvas
+$(window).resize(function() {
+    var data = canvas.toDataURL();
+
+    canvas.width *= 0.5;
+    canvas.height *= 0.5;
+
+    var img = new Image();
+
+    img.onload=function(){
+	context.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.wdith, canvas.height);};
+    
+    img.src = data;
+});
